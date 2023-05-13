a0d891d5d8b8fd05dcbff5ebced2ab9d9b7eccd7
satrtingpyth <varadavnm@gmail.com>
2023-05-14 00:00:46 +0530

    Added dart project

--- dart/datatype/bin/datatype.dart
+++ dart/datatype/bin/datatype.dart
@@ -1,5 +1,9 @@
+import 'dart:ffi';
+
 import 'package:datatype/datatype.dart' as datatype;
 
-void main(List<String> arguments) {
-  print('Hello world: ${datatype.calculate()}!');
+void main() {
+  Map<String, int> map1 = {"Age":25};
+  print(map1);
+
 }
